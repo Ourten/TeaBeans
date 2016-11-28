c8c1cf3a35ade17201b84a2bc263ed34b291c636
Phenix246 <florian.gomes246@gmail.com>
2016-11-28 11:26:29 +0100

    Implements CopyOnWriteArrayList

---
+++ src/main/java/fr/ourten/teabeans/value/CopyOnWriteArrayListProperty.java
@@ -0,0 +1,25 @@
+package fr.ourten.teabeans.value;
+
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+/**
+ * Implementation of ListProperty using CopyOnWriteArrayList
+ * 
+ * @author Phenix246
+ */
+public class CopyOnWriteArrayListProperty<T> extends ListProperty<T>
+{
+
+    public CopyOnWriteArrayListProperty(final List<T> value, final String name)
+    {
+        super(value != null ? Lists.newCopyOnWriteArrayList(value) : Lists.newCopyOnWriteArrayList(), name);
+    }
+
+    public CopyOnWriteArrayListProperty(final List<T> value)
+    {
+        this(value, "");
+    }
+
+}
